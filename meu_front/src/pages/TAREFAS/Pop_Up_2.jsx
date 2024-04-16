6cc813556a468f5d1f2a26a22a6ff83404d30f7a
Maria9713 <mvtfernandes2outlook.com>
2024-04-15 21:20:35 -0300

    POP-UP TAREFAS 3 & 4

--- meu_front/src/pages/TAREFAS/Pop_Up_2.jsx
+++ meu_front/src/pages/TAREFAS/Pop_Up_2.jsx
@@ -1,76 +1,105 @@
-
-import React from 'react';
-import { Modal, Box, Typography, Button, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Modal, Typography, Button, Grid } from '@mui/material';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import PopUp3 from './Pop_Up_3';
 
-const Popup = ({ open, handleClose }) => {
-    return (
-            <Modal open={open} onClose={handleClose}>
-            <div style={{ 
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              backgroundColor: 'white',
-              padding: '20px',
-              width: '340px',
-              height: '340px',
-              borderRadius: '6px'
-            }}>
-               <DeleteForeverOutlinedIcon sx={{ width: 300, height: 120, color: '#07382E' }} />
-              <Typography variant="h5"sx={{ textAlign:'center', fontSize:'1.5rem',fontWeight: 'bold'}}>Excluir Tarefa?  </Typography>
-              <Typography variant="body2"sx={{ marginTop:'5%',textAlign:'center', fontSize:'1rem'}}>Certeza que deseja excluir esta tarefa?  </Typography>
-              <Grid container spacing={2} sx={{marginTop: '6%'}}>
-  <Grid item xs={6}>
-  <Button
-  onClick={handleClose}
-  type="submit"
-  fullWidth
-  variant="outlined"
-  sx={{
-    mt: 2,
-    mb: 5,
-    maxWidth: "180px",
-    backgroundColor: "#FFFFFF",
-    color: "#07382E",
-    borderColor: "#07382E",
-    "&:hover": { backgroundColor: "#07382E", color: "#FFFFFF" }
-  }}
->
-  Voltar
-</Button>
-</Grid>
+const PopUp2 = ({ open, handleClose }) => {
+  const [openPopup, setOpenPopup] = useState(false);
 
-  <Grid item xs={6}>
-  <Button
-  onClick={handleClose}
-  type="submit"
-  fullWidth
-  variant="outlined"
-  sx={{
-    mt: 2,
-    mb: 5,
-    maxWidth: "180px",
-    backgroundColor: "transparent",
-    color: "#FF0000",
-    borderColor: "#FF0000",
-    "&:hover": {
-      backgroundColor: "#FF0000",
-      color: "#FFFFFF",
-      borderBlockColor: 'transparent'
-    }
-  }}
->
-  EXCLUIR
-</Button>
+  const handleOpenPopup = () => {
+    setOpenPopup(true);
+  };
 
-  </Grid>
-</Grid>
-              {/* <Button onClick={handleClose} sx={{  color: '#07382E',fontWeight: 'bold', fontSize:'1rem', marginTop: '8%', marginLeft:'70%'}}>SIM</Button>
-              <Button onClick={handleClose} sx={{  color: '#07382E',fontWeight: 'bold', fontSize:'1rem', marginTop: '0%', marginLeft:'0%'}}>Não</Button> */}
-            </div>
-          </Modal>
-    );
+  const handleClosePopup2 = () => {
+    setOpenPopup(false);
+  };
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: 'white',
+          padding: '20px',
+          width: '340px',
+          height: '340px',
+          borderRadius: '6px'
+        }}
+      >
+        <DeleteForeverOutlinedIcon
+          sx={{ width: 300, height: 120, color: '#07382E' }}
+        />
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: 'center',
+            fontSize: '1.5rem',
+            fontWeight: 'bold'
+          }}
+        >
+          Excluir Tarefa?
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ marginTop: '5%', textAlign: 'center', fontSize: '1rem' }}
+        >
+          Certeza que deseja excluir esta tarefa?
+        </Typography>
+        <Grid container spacing={2} sx={{ marginTop: '6%' }}>
+          <Grid item xs={6}>
+            <Button
+              onClick={handleClose}
+              type="submit"
+              fullWidth
+              variant="outlined"
+              sx={{
+                mt: 2,
+                mb: 5,
+                maxWidth: '180px',
+                backgroundColor: '#FFFFFF',
+                color: '#07382E',
+                borderColor: '#07382E',
+                '&:hover': {
+                  backgroundColor: '#07382E',
+                  color: '#FFFFFF'
+                }
+              }}
+            >
+              Voltar
+            </Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="outlined"
+              sx={{
+                mt: 2,
+                mb: 5,
+                maxWidth: '180px',
+                backgroundColor: 'transparent',
+                color: '#FF0000',
+                borderColor: '#FF0000',
+                '&:hover': {
+                  backgroundColor: '#FF0000',
+                  color: '#FFFFFF',
+                  borderBlockColor: 'transparent'
+                }
+              }}
+              onClick={handleOpenPopup}
+            >
+              EXCLUIR
+            </Button>
+          </Grid>
+        </Grid>
+        <PopUp3 open={openPopup} handleClose={handleClosePopup2} />
+      </div>
+    </Modal>
+  );
 };
 
-export default Popup;
+export default PopUp2;
---
+++ meu_front/src/pages/TAREFAS/Pop_Up_3.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Modal, Typography, Button } from '@mui/material';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+
+const PopUp3 = ({ open, handleClose }) => {
+  return (
+    <Modal open={open} onClose={handleClose} onClick={handleClose}>
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: 'white',
+          padding: '20px',
+          width: '340px',
+          height: '340px',
+          borderRadius: '6px',
+        }}
+      >
+        <DeleteForeverOutlinedIcon
+          sx={{ width: 300, height: 170, color: '#07382E' }}
+        />
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: 'center',
+            fontSize: '1.5rem',
+            fontWeight: 'bold'
+          }}
+        >
+          Tarefa Excluída
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ marginTop: '5%', textAlign: 'center', fontSize: '1rem' }}
+        >
+          A tarefa foi excluída com sucesso!
+        </Typography>
+      </div>
+    </Modal>
+  );
+};
+
+export default PopUp3;
